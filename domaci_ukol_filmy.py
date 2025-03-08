a71b54b955feb10553abcbcb0cfbc69bbe04c581
Mopenzi <merny.cichal@seznam.cz>
2025-03-08 09:42:26 +0100

    vytvoril jsem soubor domaci_ukol_filmy.py a vlozil do nej zadane hodnoty

---
+++ domaci_ukol_filmy.py
@@ -0,0 +1,27 @@
+sluzby = ("dostupné filmy", "detaily filmu", "seznam režisérů")
+oddelovac = "=" * 62
+
+film_1 = {
+    "jmeno": "Shawshank Redemption",
+    "rating": "93/100",
+    "rok": 1994,
+    "reziser": "Frank Darabont",
+    "stopaz": 144
+}
+
+film_2 = {
+    "jmeno": "The Godfather",
+    "rating": "92/100",
+    "rok": 1972,
+    "reziser": "Francis Ford Coppola",
+    "stopaz": 175
+}
+
+film_3 = {
+    "jmeno": "The Dark Knight",
+    "rating": "90/100",
+    "rok": 2008,
+    "reziser": "Christopher Nolan",
+    "stopaz": 152
+}
+
