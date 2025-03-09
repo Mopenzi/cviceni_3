1df27eb62cad5082d74fe25e5f8d79cdd6add38b
Mopenzi <merny.cichal@seznam.cz>
2025-03-09 09:46:52 +0100

    dodelal jsem domaci ukol podle zadani, ktere je dost na pytel a neurcite, tak nevim, jestli to je spravne, nebo jsem uplne mimo

--- domaci_ukol_filmy.py
+++ domaci_ukol_filmy.py
@@ -25,3 +25,42 @@ film_3 = {
     "stopaz": 152
 }
 
+#sjednocení filmů do jednoho slovníku, kde jednotlivé filmy jsou puvodni slovniky
+filmy = {film_1["jmeno"]: film_1, film_2["jmeno"]: film_2, film_3["jmeno"]: film_3}
+
+#uvodni vypis pro uzivatele
+print("Vítej v našem filmovém slovníku!".upper().center(62))
+print(oddelovac)
+# tady bych normalne joinul sluzby, ale vystup ma byt misto 3. sluzby("seznam reziseru") - "doporuc film"
+seznam_sluzeb = " | ".join((sluzby[0],sluzby[1], "doporuč film")) 
+print(seznam_sluzeb.center(62))
+print(oddelovac)
+print()
+
+
+#Zobrazi dostupne filmy
+print(str(sluzby[0].capitalize() + ":").center(62))
+print(oddelovac)
+print(*filmy.keys(), sep = ", ")        # * tzv rozbalí klíče do jednotlivých argumentů 
+print(oddelovac)
+print()
+
+#Zobrazi detaily o filmu
+print(sluzby[1].capitalize() + ":")
+print(oddelovac)
+print(filmy["The Dark Knight"])
+print(oddelovac)
+print()
+
+#Zobrazi seznam reziseru
+print("Všichni režiséři:")
+print(oddelovac)
+# tady to mi asi prijde, ze chteli, abychom udelali... jinak vubec nechapu... to zadani je fakt na pytel
+reziseri = set()
+for film in filmy.keys():
+    reziseri.add(filmy[film]["reziser"])
+    
+print(reziseri)
+print(oddelovac)
+print()
+    
